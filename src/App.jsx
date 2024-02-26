13ed96cb824a43ff95c9c6f5b9f261d3f79c2aec
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-02-26 20:47:40 +0530

    added impact module

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import About from './Components/About';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Impact from './Components/Impact';
 import Partner from './Components/Partner';
 import Photochanger from './Components/Photochanger';
 const photos = ['1.jpg', '2.jpg', '3.jpg'];
+const images =['five.jpeg','one.jpeg','three.jpeg','four.jpeg']
 function App() {
   return (
     <>
       <Header></Header>
       <Photochanger photos={photos}></Photochanger>
       <About></About>
+      <Impact  images = {images}></Impact>
       <Partner></Partner>
       <Footer></Footer>
     </>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,17 @@
-import React from 'react'
-import "../Styles/Footer.css"
+import React from "react";
+import "../Styles/Footer.css";
 export default function Footer() {
   return (
-    <div className='footer'>
-        <div id='one'>
-            @ 2024 All Right Reserved
-        </div>
-        <div id='two'>
-            Khwaja Moinuddin Chishti Language University
-        </div>
-<div className='links'>
-<a href="header.jsx" class="fa fa-facebook"></a>
-<a href="header.jsx" class="fa fa-twitter"></a>
-<a href="header.jsx" class="fa fa-linkedin"></a>
-<a href="header.jsx" class="fa fa-whatsapp"></a>
-<a href="header.jsx" class="fa fa-telegram"></a>
-</div>
-
-
-
+    <div className="footer">
+      <div id="one">@ 2024 All Right Reserved</div>
+      <div id="two">Khwaja Moinuddin Chishti Language University</div>
+      <div className="links">
+        <a href="header.jsx" className="fa fa-facebook"></a>
+        <a href="header.jsx" className="fa fa-twitter"></a>
+        <a href="header.jsx" className="fa fa-linkedin"></a>
+        <a href="header.jsx" className="fa fa-whatsapp"></a>
+        <a href="header.jsx" className="fa fa-telegram"></a>
+      </div>
     </div>
-  )
+  );
 }
